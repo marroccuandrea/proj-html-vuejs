f8c9d63cdb27379c6312d7d74ec1fbd9cecf27be
marroccuandrea <marroccuandrea@yahoo.it>
2024-04-09 18:30:33 +0200

    footer dinamico

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,11 @@
 <script>
-   import {store} from '../data/store'
+   import {store} from '../data/store';
+   import {footerMenues} from '../data/menues'
     export default {  
     data(){
       return{
-        store
+        store,
+        footerMenues
       }
     }
     
@@ -12,40 +14,48 @@
 
 <template>
   <footer>
-    <div class="container">
+    <div class="container footer-top">
         <div class="row row-cols-3">
-        <div class="col text-center">
-            <img class="logo-footer mb-4" src="../../img/logo-200x34.png" alt="">
-            <p class="text-footer mb-4">Ac aliquam neque sagittis diam faucibus vitae purus turpis phasellus. Pellentesque consectetur amet purus ultrices mauris.</p>
-            <nav>
-                <ul class="footer-menu d-flex justify-content-center">
-                    <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                    <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
-                    <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-                    <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
-                </ul>
-            </nav>
-        </div>
-        <div class="col text-center">
-            <h6>RECENT POSTS</h6>
-            
-                <ul class="posts-menu">
-                    <li><a href="#"><i class="fa-solid fa-chevron-right"></i> The best protein shake</a></li>
-                    <li><a href="#"><i class="fa-solid fa-chevron-right"></i> Ultimate cardio workout</a></li>
-                    <li><a href="#"><i class="fa-solid fa-chevron-right"></i> New juices available now</a></li>
-                    <li><a href="#"><i class="fa-solid fa-chevron-right"></i> Tips to find training partners</a></li>
-                    <li><a href="#"><i class="fa-solid fa-chevron-right"></i> 20 best healthy recipes</a></li>
-                </ul>
-            
-        </div>
-        <div class="col text-center">
-            <h6 class="mb-4">CONTACT INFORMATIONS</h6>
-            <p class="text-footer">4746 Tipple Road Michigan 48449</p>
-            <h6>Mobile: 1.800.000.0000</h6>
-            <h6>Email: info@your-company.com</h6>
+            <div class="col text-center">
+                <img class="logo-footer mb-4" src="../../img/logo-200x34.png" alt="">
+                <p class="text-footer mb-4">Ac aliquam neque sagittis diam faucibus vitae purus turpis phasellus. Pellentesque consectetur amet purus ultrices mauris.</p>
+                <nav class= "d-flex justify-content-center">
+                    <ul 
+                    v-for="(item,index) in footerMenues.social" 
+                    :key="`s-${index}`" 
+                    class="footer-menu ">
+                        <li>
+                            <a :href="item.href" v-html="`${item.text}`"></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col text-center">
+                <h6>RECENT POSTS</h6>
+                
+                    <ul 
+                    v-for="(item,index) in footerMenues.posts"
+                    :key="`p-${index}`"
+                    class="posts-menu">
+                        <li>
+                            <i class="fa-solid fa-chevron-right"></i>
+                            <a :href="item.href">{{ item.text }}</a>
+                        </li>
+                    </ul>
+                
+            </div>
+            <div class="col text-center">
+                <h6 class="mb-4">CONTACT INFORMATIONS</h6>
+                <p class="text-footer">4746 Tipple Road Michigan 48449</p>
+                <h6>Mobile: 1.800.000.0000</h6>
+                <h6>Email: info@your-company.com</h6>
+            </div>
         </div>
     </div>
+    <div class="container-fluid footer-btm">
+        <div class="container h-100 d-flex justify-content-center align-items-center">
+            <p class="copyright">© Copyright 2012 - 2020 | Avada theme by <strong>ThemeFusion</strong> |  All Rights Reserved | Powered by <strong>Word Press</strong></p>
+        </div>
     </div>
     
   </footer>
@@ -56,22 +66,27 @@
 @import '../assets/scss/partials/variables.scss';
 footer{
     background-color: black;
-    ul{
+    
+    .posts-menu{
     list-style: none;
     margin-right: 15px;
+    }
+    .footer-menu{
+        list-style: none;
+        
+    }
+    li{
         a{
         color: $rosso-icone;
         text-decoration: none;
-        margin: 0 0.4rem;
         font-size: 0.8rem;
-            
         }
     }
     .col{
         margin: 100px 0;
         color: white;
         h6{
-            font-size: 0.8rem;
+            font-size: 0.9rem;
         }
         .logo-footer{
             width: 150px;
@@ -89,6 +104,17 @@ footer{
             }
             i{
                 font-size: 0.6rem;
+            }
+        }
+    }
+    .footer-btm{
+        border-top: 1px solid $grigio;
+        height: 100px;
+        .copyright{
+            color: $grigio;
+            font-size: 0.7rem;
+            strong{
+                color: white;
             }
         }
     }
--- src/data/menues.js
+++ src/data/menues.js
@@ -38,4 +38,52 @@ const headerMenues = {
     ]
 }
 
-export { headerMenues }
+const footerMenues = {
+    social: [
+        {
+            text: '<i class="fa-brands fa-facebook-f"></i>',
+            href: '#'
+        },
+        {
+            text: '<i class="fa-brands fa-twitter"></i>',
+            href: '#'
+        },
+        {
+            text: '<i class="fa-brands fa-youtube"></i>',
+            href: '#'
+        },
+        {
+            text: '<i class="fa-brands fa-instagram"></i>',
+            href: '#'
+        },
+        {
+            text: '<i class="fa-brands fa-linkedin-in"></i>',
+            href: '#'
+        }
+    ],
+    posts: [
+        {
+            text: ' The best protein shake',
+            href: '#'
+        },
+        {
+            text: ' Ultimate cardio workout',
+            href: '#'
+        },
+        {
+            text: ' New juices available now',
+            href: '#'
+        },
+        {
+            text: ' Tips to find training partners',
+            href: '#'
+        },
+        {
+            text: ' 20 best healthy recipes',
+            href: '#'
+        }
+    ]
+}
+
+
+export { headerMenues, footerMenues }
